de188c3e37324be574a9604b2105a033388b85ac
Thanit Kongsmoot <victortykung5@gmail.com>
2023-03-14 17:32:15 +0700

    delete shiping address from account

--- Account.py
+++ Account.py
@@ -15,7 +15,7 @@ class Admin(Account):
             self.__promotion = promotion # Promotion object
             self.__order = order # Order object (specific user) 
     #some method for managing our web
-        
+   
         
 class User(Account):
      def __init__(self, username, password, email, phone_number, person_data, address, cart, order,  order_history, coupon):
@@ -32,13 +32,3 @@ class User(Account):
           pass
      def get_address():
           pass
-
-class Shipping_Address:
-    def __init__(self, name_surname, phone_number, address, sub_district, district, province, postal_code):
-        self.__name_surname = name_surname
-        self.__phone_number = phone_number
-        self.__address = address
-        self.__sub_district = sub_district
-        self.__district = district
-        self.__province = province
-        self.__postal_code = postal_code
