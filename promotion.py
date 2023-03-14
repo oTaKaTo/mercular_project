9a4a15bbf9a3e4d09b57c32c81cc90f31f8f074f
Chaksawat <Chaksawat2546@gmail.com>
2023-03-14 17:35:52 +0700

    <fix> add proper method to classes

--- promotion.py
+++ promotion.py
@@ -1,10 +1,9 @@
 class Promotion:
-    def __init__(self, duration, description, relevant_products):
+    def __init__(self, duration, description):
         self.__duration = duration
         self.__description = description
-        self.__relevant_products = relevant_products
 
-    def is_available(duration):
+    def is_available(self):
         pass
 
 
@@ -13,11 +12,17 @@ class FlatDiscount(Promotion):
         Promotion.__init__(self, duration, description)
         self.__discount = discount
 
+    def get_discount():
+        pass
+
 
 class PercentageDiscount(Promotion):
-    def __init__(self, duration, description, discount_percent):
+    def __init__(self, duration, description, discount_percent, max_discount):
         Promotion.__init__(self, duration, description)
         self.__discount_percent = discount_percent
+    
+    def get_discount():
+        pass
 
 
 class Coupon(Promotion):
@@ -26,6 +31,9 @@ class Coupon(Promotion):
         self.__discount = discount
         self.__quantity = quantity
         self.__code_id = code_id
+    
+    def get_discount():
+        pass
 
 
 class CouponCatalog():
