c2ca325734617148bc9dc24c60bba5636cf3d6d5
Thanit Kongsmoot <victortykung5@gmail.com>
2023-03-14 17:50:52 +0700

    <feat> add_shippingaddress class

---
+++ Shipping_Address.py
@@ -0,0 +1,9 @@
+class Shipping_Address:
+    def __init__(self, name_surname, phone_number, address, sub_district, district, province, postal_code):
+        self.__name_surname = name_surname
+        self.__phone_number = phone_number
+        self.__address = address
+        self.__sub_district = sub_district
+        self.__district = district
+        self.__province = province
+        self.__postal_code = postal_code
