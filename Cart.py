3301a66999b21d34b2c1b4416a8bf7c7c72d3e4e
Peqchji <110451238+Peqchji@users.noreply.github.com>
2023-03-14 17:06:15 +0700

    <feat> Create Cart Class

---
+++ Cart.py
@@ -0,0 +1,20 @@
+class Cart:
+     def __init__(self, items, total_price, coupon):
+        self.__items = [] # List for store Item object 
+        self.__total_price = 0
+        self.__coupon = coupon # Coupon object (use a coupon for discount)
+     
+     def get_cart():
+          pass
+     
+     def get_is_selected_item():
+          pass
+     
+     def add_item_to_cart(item):
+          pass
+     
+     def update_total_item():
+          pass
+     
+     def update_total_price(coupon):
+          pass
