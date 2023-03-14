e08cb9474b5119014d01976e6cb3c26a5d891558
Thanit Kongsmoot <victortykung5@gmail.com>
2023-03-14 17:28:04 +0700

    <feat> Create Account class

---
+++ Account.py
@@ -0,0 +1,44 @@
+class Account:
+    def __init__(self, username, password, email, phone_number):
+        self.__username = username
+        self.__password = password #must be hash before store
+        self.__email = email
+        self.__phone_number = phone_number
+
+class Admin(Account):
+    __product_catalog = []
+
+    def __init__(self, username, password, email, phone_number, product_catalog, coupon, promotion, order):
+            Account.__init__(self, username, password, email, phone_number)
+            self.__product_catalog = product_catalog # ProductCataog object
+            self.__coupon = coupon # Coupon object
+            self.__promotion = promotion # Promotion object
+            self.__order = order # Order object (specific user) 
+    #some method for managing our web
+        
+        
+class User(Account):
+     def __init__(self, username, password, email, phone_number, person_data, address, cart, order,  order_history, coupon):
+        Account.__init__(self, username, password, email, phone_number)
+        self.__person_data = person_data
+        self.__address = [] # List of Shipping_Address Object 
+        self.__cart = cart # Cart object
+        self.__order_history = order_history # OrderHistory object
+        self.__coupons = [] # List for store Coupon object
+     
+     def get_user_data():
+          pass
+     def get_user_coupon():
+          pass
+     def get_address():
+          pass
+
+class Shipping_Address:
+    def __init__(self, name_surname, phone_number, address, sub_district, district, province, postal_code):
+        self.__name_surname = name_surname
+        self.__phone_number = phone_number
+        self.__address = address
+        self.__sub_district = sub_district
+        self.__district = district
+        self.__province = province
+        self.__postal_code = postal_code
