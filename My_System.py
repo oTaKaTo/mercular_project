43d511e778920ecd2952bfc1aa1004903141903e
Thanit Kongsmoot <victortykung5@gmail.com>
2023-03-25 20:35:36 +0700

    change User_dic to list that has product

--- My_System.py
+++ My_System.py
@@ -1,38 +1,34 @@
-user_dic = {}
-def Register(email,password):
-    for name in user_dic:
-        if name == email:
-            print ("Email is already use")
-            break;
-    else:
-        user_dic[email] = password
+from Account import *
+user_lst = []
+def Register(email,username,password):
+        for ID in user_lst:
+            if ID.get_Email() == email:
+                print ("Email is already use")
+                break;
+        else:
+                user_lst.append(User(username,password,email))
 
 def Login(email,password):
-    for name in user_dic:
-        if name == email and user_dic[name]==password:
-            print("Login success")
-            break;
-    else:
-            print("invail Email or password")
-def Logout():
-    print("Logout Confirm\nY/N")
-    while(True):
-        Input = input()
-        if  Input=="Y":
-            print("Logout Success")
-            break;
-        elif Input=="N":
-            print("Continue Login")
-            break;
+        for ID in user_lst:
+            if (ID.get_Email() == email)  and ID.get_password()==password:
+                print("Login success")
+                break;
         else:
-            print("Error!!!!\nPlease Enter Y or N")
-
-Register("momo@gmail.com","1234")
-Register("2321231@gmail.com","5678")
-Register("uhkjhjkjh@gmail.com","91011")
-Register("momo@gmail.com","126534")
-print(user_dic)
+                print("invail Email or password")
+def Logout():
+        print("Logout Confirm\nY/N")
+        while(True):
+            Input = input()
+            if  Input=="Y":
+                print("Logout Success")
+                break;
+            elif Input=="N":
+                print("Continue Login")
+                break;
+            else:
+                print("Error!!!!\nPlease Enter Y or N")
+Register("momo@gmail.com","momo","1234")
+Register("momo@gmail.com","momo","1234")
 Login("momo@gmail.com","1234")
-Login("momo123@gmail.com","1234")
-Login("momo@gmail.com","12345")
-Logout()
+
+
