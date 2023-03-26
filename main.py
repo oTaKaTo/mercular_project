b2c421be8270b2b238f60b5049bcc6c28d5aeb86
Peqchji <110451238+Peqchji@users.noreply.github.com>
2023-03-27 00:04:14 +0700

    <feat> add select_item and get_selected_item()

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# from Product import ProductCatalog
-# import System
-
-# def main():
-#     product_catalog = ProductCatalog()
-#     web_marketplace_system = System(product_catalog)
-    
-# if __name__ == "__main__":
-#     main()
 from Product import Product
 from System import System
 from Account import User
