4a64db56fad5ee65506b70b60968113eb950d890
Thanit Kongsmoot <victortykung5@gmail.com>
2023-03-28 17:21:11 +0700

    add set_shipping_address,get_name_surname,get_address,get_phone_number,get_subdistrict_,get_province,get_postal_code

--- Shipping_Address.py
+++ Shipping_Address.py
@@ -7,3 +7,28 @@ class Shipping_Address:
         self.__district = district
         self.__province = province
         self.__postal_code = postal_code
+    def set_shipping_address(self,new_name_surname,new_phone_number,new_address,new_sub_district,new_district,new_provine,new_postal_code):
+        self.__name_surname= new_name_surname
+        self.__phone_number = new_phone_number
+        self.__address= new_address
+        self.__sub_district= new_sub_district
+        self.__district= new_district
+        self.__province = new_provine
+        self.__postal_code = new_postal_code
+    def get_name_surname(self):
+        return self.__name_surname
+    def get_phone_number(self):
+        return self.__phone_number
+    def get_address(self):
+        return self.__address
+    def get_sub_district(self):
+        return self.__sub_district
+    def get_district(self):
+        return self.__district
+    def get_province(self):
+        return self.__province
+    def get_postal_code(self):
+        return self.__postal_code
+        
+        
+    
