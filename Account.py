d27b2e8d883084207d744ca9785daa4c8d01b5a4
Thanit Kongsmoot <victortykung5@gmail.com>
2023-03-28 17:18:50 +0700

    add online_status,get_online_Status,set_online_status,set_online_status,set_username,get_phone_number,set_phone_number,get_person_data,get_cart,_getorder_history,get_coupon edit add_address

--- Account.py
+++ Account.py
@@ -1,17 +1,47 @@
-from Product import Product
+from Product import *
+from Shipping_Address import *
 class Account:
-     def __init__(self, username, password, email, phone_number):
+     def __init__(self, username, password, email, phone_number,online_status:bool=None):
           self.__username = username
           self.__password = password #must be hash before store
           self.__email = email
           self.__phone_number = phone_number
+          self.__online_status = online_status
+    
      def get_Email(self):
           return self.__email
+    
      def get_username(self):
           return self.__username
+     
+     def get_online_status(self):
+          return self.__online_status
+    
+     def set_online_status(self,new_online_status):
+          if isinstance(new_online_status,bool):
+               self.__online_status = new_online_status
+               return True
+          return False
+     
+     def set_username(self,new_username):
+          if isinstance(new_username,str):
+               self.__username = new_username
+               return True
+          return False
+     
      def get_password(self):
           return self.__password
      
+     def get_phone_number(self):
+          return self.__phone_number
+     
+     def set_phone_number(self,new_phone_number):
+          if isinstance(new_phone_number,int):
+               self.__phone_number = new_phone_number
+               return True
+          return False          
+
+         
 class Admin(Account):
      __product_catalog = []
      def __init__(self, username, password, email, phone_number, product_catalog, coupon, promotion, order):
@@ -36,12 +66,35 @@ class User(Account):
           return self.__coupons
      def get_address(self):
           return self.__address
-     def add_address(self,address):
-          self.__address.append(address)
+     def get_person_data(self):
+          return self.__person_data
+     def get_cart(self):
+          return self.__cart
+     def get_order_history(self):
+          return self.__order_history
+     def get_coupons(self):
+          return self.__coupons
+     def add_address(self,name_surname, phone_number, address, sub_district, district, province, postal_code):
+          self.__address.append(Shipping_Address(name_surname,phone_number,address,sub_district,district,province,postal_code))
           return True
+     
      def delete_address(self,address):
           if len(self.__address)>1:
                self.__address.remove(address)
-               return True
+               return "Remove address success"
           else:
                return"You must have at least 1 address for delivery"
+
+"""momo = User("momo","1234","65010244@gmail.com")
+momo.add_address("ที่อยู่แรก","adsad","Sdadsad","ASdasdsa","asdasdsa","asdasdsa","Adsadsa")
+momo.add_address("ที่อยู่2","adsad","Sdadsad","ASdasdsa","asdasdsa","asdasdsa","Adsadsa")
+print([i.get_name_surname() for i in momo.get_address()])
+print("-----------Add Address-----------")
+momo.get_address()[0].set_shipping_address("ที่อยู่3","adsad","Sdadsad","ASdasdsa","asdasdsa","asdasdsa","Adsadsa")
+print([i.get_name_surname() for i in momo.get_address()])
+print("-----------Set Address-----------")
+momo.delete_address(momo.get_address()[0])
+print([i.get_name_surname() for i in momo.get_address()])
+print(momo.delete_address(momo.get_address()[0]))
+print([i.get_name_surname() for i in momo.get_address()])
+print("-----------Delete Address-----------")"""
