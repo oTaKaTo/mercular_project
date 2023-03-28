b6f3f87aba3c17e471031b24307465c65a1697fa
Thanit Kongsmoot <victortykung5@gmail.com>
2023-03-28 17:19:59 +0700

    change mechnic of create_account,,login,logout add get_user_lst

--- My_System.py
+++ My_System.py
@@ -2,37 +2,56 @@ from Account import *
 class System:
     def __init__(self):
         self.__user_lst = []
+
+    def get_user_lst(self):
+        return self.__user_lst
+        
     def create_account(self,username, password, email, phone_number=None):
-            if self.check_exists_account(email):
-                return False
+            if self.check_exists_account(email)==True:
+                return "already has account using this email"
             else:
                 self.__user_lst.append(User(username,password,email,phone_number))
-                return True
+                self.login(email,password)
+                return "Create account successfully"
             
     def check_exists_account(self,email):
             for ID in self.__user_lst:
                 if ID.get_Email() == email:
                     return True
             return False
-    
+   
+    def login(self,email,password):
+        ID = self.check_id_password(email,password)
+        if ID != False:
+            ID.set_online_status(True)
+            return f"online statue = {ID.get_online_status()}"
+        return "email or password invaild"
+   
     def check_id_password(self,email, password):
             for ID in self.__user_lst:
-               if ID.get_Email==email and ID.get_password == password:
-                   return True
+               if ID.get_Email()==email and ID.get_password() == password:
+                   return ID
             return False
     
-    def Logout(self,ID):
-            pass
-        
+    def logout(self,ID):
+            ID.set_online_status(False)
+            return True
+            
 mySystem = System()
-print(mySystem.create_account("momo","1234","MOmo@gmail.com"))
-print(mySystem.create_account("momo","1234","MOmo@gmail.com"))
-print(mySystem.check_exists_account("MOmo@gmail.com"))
-print(mySystem.check_exists_account("MOmo@gmaisl.com"))      
-print(mySystem.check_id_password("MOmo@gmail.com","1234"))    
-print(mySystem.check_id_password("MOmo@gmail.com","123s4"))   
-print(mySystem.check_id_password("MOmsdasado@gmail.com","123s4")) 
-
+#create Account
+mySystem.create_account("momo","1234","MOMO@gmail.com")
+#print(mySystem.get_user_lst())
+#print(mySystem.create_account("momo","1234","MOMO@gmail.com"))
+#login
+#print(mySystem.create_account("momo","1234","MOMO@gmail.com"))
+#print(mySystem.login("MOMO@gmail.com","1234"))
+#print(mySystem.login("MOMO3@gmail.com","1234"))
+#print(mySystem.login("MOMO@gmail.com","12345678"))
+#logout
+account_list = mySystem.get_user_lst()
+print(account_list[0].get_online_status())
+mySystem.logout(mySystem.get_user_lst()[0])
+print(account_list[0].get_online_status())
 """  
     def Register(email,username,password):
             for ID in self.user_lst:
