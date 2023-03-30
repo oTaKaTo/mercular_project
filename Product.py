ec54d93a542c3864652e802992cadaa2b3c3d9a1
Takdanai Deephuak <65010373@kmitl.ac.th>
2023-03-30 23:04:12 +0700

    <feat>add search by catagories,get type(Product)

--- Product.py
+++ Product.py
@@ -8,9 +8,6 @@ class ProductCatalog:
         self.__products[product.get_product_id()] = product
         return True
 
-    def search_by_id(self, product_id):
-        return self.__products[product_id]
-
     def remove_product(self, product_id):
         if product_id in self.__products:
             del self.__products[product_id]
@@ -23,28 +20,37 @@ class ProductCatalog:
             self.__products[id] = product
             return True
         return False
-    
-    def add_promotion(self,product_id,promotion):
+
+    def add_promotion(self, product_id, promotion):
         self.__products[product_id].add_promotion(promotion)
         return True
 
     def get_product_info(self):
         return [self.__products[x].get_name() for x in self.__products]
-    
+
     def update_quantity(self, quantity):
         pass
-    
-    
-    def search_keyword(self,keyword=""):
+
+    def search_keyword(self, keyword=""):
         search_result = []
         for i in self.__products:
-            print(keyword.lower(), (self.__products[i].get_name()).lower() )
             if keyword.lower() in (self.__products[i].get_name()).lower():
                 search_result.append(self.__products[i])
             if keyword.lower() == self.__products[i].get_product_id():
-                return self.__products[i]   
+                return self.__products[i]
         return search_result
-                
+
+    def search_by_id(self, product_id):
+        return self.__products[product_id]
+    
+    def search_by_catagories(self,keyword=""):
+        search_result = []
+        for i in self.__products:
+            for t in reversed((self.__products[i].get_type())):
+                if keyword == t:
+                    search_result.append(self.__products[i])
+        return search_result
+
     
 class Item:
     def __init__(self, product, quantity):
@@ -67,14 +73,14 @@ class Item:
 
 
 class Product:
-    def __init__(self, product_id, object_id, name, type, brand, price,amount, detail="",image=[], option=[]):
+    # type MUST order by big --> small catagory 
+    def __init__(self, product_id, object_id, name, type:list, brand, price, detail="",image=[], option=[]):
         self.__product_id = product_id
         self.__object_id = object_id
         self.__name = name
         self.__type = type
         self.__brand = brand
         self.__price = price
-        self.__amount = amount
         self.__image = image
         self.__option = option
         self.__detail = detail
@@ -90,6 +96,9 @@ class Product:
     def get_product_id(self):
         return str(self.__product_id)
     
+    def get_type(self):
+        return self.__type
+
     def get_type_brand_id(self):
         return {"type": self.__type, "brand": self.__brand, "id": self.__product_id}
 
