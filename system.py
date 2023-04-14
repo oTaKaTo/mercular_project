14ff3056b454a445d3cabdc5279bce02572bdc78
Thanit Kongsmoot <victortykung5@gmail.com>
2023-04-15 04:40:51 +0700

    <feats> add editphonenumber add addcoupon add view coupon add used coupon

--- system.py
+++ system.py
@@ -1,5 +1,5 @@
 from account import Account,User,Admin,shipping_adress
-from promotion import Promotion,Coupon,CouponCatalog
+from promotion import Promotion,Coupon,CouponCatalog,PercentageCoupon,PercentageDiscount,FlatCoupon,FlatDiscount
 from Product import Product,Item,ProductCatalog
 from tkinter import Tk
 from fastapi import FastAPI
@@ -17,6 +17,12 @@ class System:
             if ID.get_email() == email:
                 return ID
         return False
+   
+    def search_coupon_by_coupon_id(self,coupon_id):
+        for coupon in self.__coupon_catalog.get_coupons():
+            if coupon.get_id() == coupon_id:
+                return coupon
+        return False
         
     def get_user_lst(self):
         return self.__user_lst
@@ -31,7 +37,7 @@ class System:
                 self.__user_lst.append(User(username,password,email,phone_number))
                 self.login(email,password)
                 return True
-    
+        
     def get_product_catalog(self):
         return self.__product_catalog        
     
@@ -57,113 +63,49 @@ class System:
         
         
 x = shipping_adress("HO","225","SAdasd","asdsad","sada","ASDASDS","asdasdsad")
+Coupon_test = FlatCoupon("26-4-2023", 100, 50, 1)
+my_pc_coupon = PercentageCoupon("23-4-2023", 100, 20, 10, 1)
 mySystem = System()
-mySystem.create_account("MOMO@gmail.com","1234")
-mySystem.search_user_by_email("MOMO@gmail.com").add_address("1234","fgfgs","SAdasd","asdsad","sada","ASDASDS","asdasdsad")
-mySystem.search_user_by_email("MOMO@gmail.com").add_address("5678","sdsds","SAdasd","asdsad","sada","ASDASDS","asdasdsad")
-print(mySystem.login("MOMO@gmail.com","1234"))
-print(mySystem.check_id_password("MOMO@gmail.com","1234"))
-"""price= 300
-my_coupon = FlatCoupon("26-4-2023", 100, 50, 1)
-my_pc_coupon = PercentageCoupon("23-4-2023", 100, 20, 10, 1)
-mySystem.get_coupon_catalog().add_coupon(my_coupon)
+mySystem.get_coupon_catalog().add_coupon(Coupon_test)
 mySystem.get_coupon_catalog().add_coupon(my_pc_coupon)
-print(mySystem.get_coupon_catalog().get_coupon())
-mySystem.get_user_lst()["MOMO@gmail.com"].add_user_coupon("2",mySystem.get_coupon_catalog().get_coupon())
-mySystem.get_user_lst()["MOMO@gmail.com"].get_user_coupon()
-print(mySystem.get_user_lst()) 
-print(mySystem.create_account("momo","1234","MOMO@gmail.com"))
-print(mySystem.create_account("momo","1234","MOMO@gmail.com"))
-print(mySystem.login("MOMO@gmail.com","1234"))
-print(mySystem.login("MOMO3@gmail.com","1234"))
-print(mySystem.login("MOMO@gmail.com","12345678"))
-account_list = mySystem.get_user_lst()
-print(account_list["MOMO@gmail.com"].get_online_status())
-mySystem.logout(mySystem.get_user_lst()["MOMO@gmail.com"])
-print(account_list["MOMO@gmail.com"].get_online_status())"""
-
-"""  
-    def Register(email,username,password):
-            for ID in self.user_lst:
-                if ID.get_email() == email:
-                    print ("Email is already use")
-                    break;
-            else:
-                    user_lst.append(User(username,password,email))
-
-    def Login(email,password):
-            for ID in user_lst:
-                if (ID.get_email() == email)  and ID.get_password()==password:
-                    print("Login success")
-                    break;
-            else:
-                    print("invail Email or password")
-    def Logout():
-            print("Logout Confirm\nY/N")
-            while(True):
-                Input = input()
-                if  Input=="Y":
-                    print("Logout Success")
-                    break;
-                elif Input=="N":
-                    print("Continue Login")
-                    break;
-                else:
-                    print("Error!!!!\nPlease Enter Y or N")
-Register("momo@gmail.com","momo","1234")
-Register("momo@gmail.com","momo","1234")
-Login("momo@gmail.com","1234")
-"""
-#test coupon
-"""
-#test admin class
-price= 300
-my_coupon = FlatCoupon("26-4-2023", 100, 50, 1)
-my_pc_coupon = PercentageCoupon("23-4-2023", 100, 20, 10, 1)
-my_coupon_catalog = CouponCatalog()
-my_coupon_catalog.add_coupon(my_coupon)
-my_coupon_catalog.add_coupon(my_pc_coupon)
-mySystem.get_user_lst()[0].add_user_coupon("2",my_coupon_catalog.get_coupon())
-print(mySystem.get_user_lst()[0].add_user_coupon("2",my_coupon_catalog.get_coupon()))
-print(mySystem.get_user_lst()[0].get_user_coupon()[0].get_id())
-print(mySystem.get_user_lst()[0].user_used_coupon(mySystem.get_user_lst()[0].get_user_coupon()[0]))
-print(mySystem.get_user_lst()[0].get_used_user_coupons())
-mySystem.get_user_lst().append(Admin("Admin","Admin","Admin@gmail.com"))
-"""
-
+mySystem.create_account("MOMO@gmail.com","1234")
+print(mySystem.search_user_by_email("MOMO@gmail.com").add_user_coupon("2",mySystem.get_coupon_catalog().get_coupons()))
 app = FastAPI()
-
 @app.get("/")
 def read_root():
     return {"Hello": "World My friend"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = "Hi"):
-    return {"item_id": item_id, "q": q}
+@app.get("/{email}/account/profile",tags=["account"])
+def read_user_infor(email:str):
+    id = mySystem.search_user_by_email(email)
+    if id!=False:
+        return {
+                "Username":id.get_username(),
+                "PhoneNumber":id.get_phone_number(),
+                "Email":id.get_email()
+                }
+    else:
+        return {"Failed":"ID not Found"}
 
-@app.get("/account/profile",tags=["account"])
-def read_user_infor():
-    return {
-            "Username":mySystem.search_user_by_email("MOMO@gmail.com").get_username(),
-            "PhoneNumber":mySystem.search_user_by_email("MOMO@gmail.com").get_phone_number(),
-            "Email":mySystem.search_user_by_email("MOMO@gmail.com").get_email()
-            }
-
-@app.get("/account/shipping_address",tags=["account"])
-def read_user_shipping_address_infor():
+@app.get("/{email}/account/shipping_address",tags=["account"])
+def read_user_shipping_address_infor(email:str):
     shipping_address_dict = {}
-    k= 1
-    for i in mySystem.search_user_by_email("MOMO@gmail.com").get_address():
-        x = {"name_surname" : i.get_name_surname(),
-             "phone_number":i.get_phone_number(),
-             "address":i.get_address(),
-             "sub_district":i.get_sub_district(),
-             "district":i.get_district(),
-             "province":i.get_province(),
-             "postal_code":i.get_postal_code()}
-        shipping_address_dict[f"Address{k}"] = x
-        k +=1
-    return shipping_address_dict 
+    id = mySystem.search_user_by_email(email)
+    if id!=False:
+        k= 1
+        for i in id.get_address():
+            x = {"name_surname" : i.get_name_surname(),
+                "phone_number":i.get_phone_number(),
+                "address":i.get_address(),
+                "sub_district":i.get_sub_district(),
+                "district":i.get_district(),
+                "province":i.get_province(),
+                "postal_code":i.get_postal_code()}
+            shipping_address_dict[f"Address{k}"] = x
+            k +=1
+        return shipping_address_dict
+    else:
+        return {"failed":"ID not found"} 
 
 @app.post("/login",tags=["account"])
 def  login(data:dict):
@@ -184,7 +126,6 @@ def register(data:dict):
         return {"Register":len(mySystem.get_user_lst())}
     else :
         return {"Register":"failed email is already use"}
-
 
 @app.post("/{email}/account/add_shipping_address" , tags=["account"])
 def add_shipping_address(email:str,data:dict):
@@ -248,3 +189,65 @@ def edit_username(email:str,data:dict):
             return {"failed":"Something but i dont know"}
     else:
         return {"failed":"email not found"}
+
+@app.put("/{email}/account/edit_phonenumber",tags=["account"])
+def edit_phonenumber(email:str,data:dict):
+    new_phone_number = data["new_phone_number"]
+    id = mySystem.search_user_by_email(email)
+    if id!=False:
+        if id.edit_phone_number(new_phone_number):
+            return {"Success":f"edit Phone-Number form {email}"}
+        else:
+            return{"Failed":f"Phone_Number False"}
+    else:
+        return{"Failed":"Email not found"}
+
+@app.post("/{email}/account/add_coupon",tags=["account"])
+def add_coupon(email:str,data:dict):
+    new_coupon = data["new_coupon"]
+    id = mySystem.search_user_by_email(email)
+    if id!=False:
+        response = id.add_user_coupon(new_coupon,mySystem.get_coupon_catalog().get_coupons())
+        if response == True:
+            return {"Success":f"add Coupon ID {new_coupon}"}
+        else:
+            return {"Failed":f"{response}"}
+    else:
+        return {"Failed":f"Email not found"}
+
+@app.get("/{email}/account/view_user_coupon",tags=["account"])
+def view_user_coupon(email:str):
+    id = mySystem.search_user_by_email(email)
+    if id!=False:
+        user_coupon_dict = {}
+        user_coupon_id = []
+        for coupon in id.get_user_coupon():
+            user_coupon_id.append(coupon)
+        user_coupon_dict["user_coupon"] = user_coupon_id
+        used_coupon =[]
+        for coupon in id.get_used_user_coupon():
+            used_coupon.append(coupon)
+        user_coupon_dict["used_coupon"] = used_coupon
+        expire_coupon =[]
+        for coupon in id.get_expire_coupon():
+            expire_coupon.append(coupon)
+        user_coupon_dict["expire_coupon"] = expire_coupon
+        return user_coupon_dict
+    else:
+        return {"Failed":"Email Not Found"}
+
+@app.put("/{email}/account/user_used_coupon",tags=["account"])
+def  user_used_coupon(email:str,data:dict):
+    used_coupon_id = data["used_coupon_id"]
+    id = mySystem.search_user_by_email(email)
+    if id != False:
+        used_coupon = mySystem.search_coupon_by_coupon_id(used_coupon_id)
+        if used_coupon!=False:    
+            if id.user_used_coupon(used_coupon):
+                return {"Success":f"Use coupon {used_coupon.get_id()}"}
+            else:
+                return {"Failed":f"you has already used this coupon"}
+        else:
+            return {"Failed":"Coupon Not Found"}
+    else:
+        return {"Failed":"Email Not Found"}
